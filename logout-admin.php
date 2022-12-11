998b39d1632811c1ef8acb4e74f4192a5e43603d
iamcathdizon <catherindizon14@gmail.com>
2022-12-11 20:56:47 +0800

    logout-admin

---
+++ logout-admin.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/admin-panel.css">
+    <title>Log Out | Tiny Boutique </title>
+</head>
+<body>
+    <div id="throbber" style="display:none; min-height:120px;"></div>
+    <div id="noty-holder"></div>
+    <div id="wrapper">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <i class="fa fa-store"></i>
+                    Tiny Boutique 
+                </a>
+            </div>
+            <ul class="nav navbar-right top-nav">           
+                <li class="dropdown">
+                    <a href="#" class="" data-toggle="dropdown">Bye, Admin!</a>
+                </li>
+            </ul>
+            <div class="row" id="main" >
+                    <div class="col-sm-12 col-md-12 well" id="content">
+                        <h1>Account Successfully Log out!</h1>
+                        <a href="index.php" class="btn btn-success">Continue</a>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+</body>
+</html>
